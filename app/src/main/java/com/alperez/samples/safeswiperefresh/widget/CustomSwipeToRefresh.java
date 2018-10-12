50e96787acb3c4e9e6d8be30c989e477174437ab
stanislav.perchenko <lkjnbv123>
2018-10-12 13:15:59 +0300

    CustomSwipeToRefresh from Stackoverflow

---
+++ app/src/main/java/com/alperez/samples/safeswiperefresh/widget/CustomSwipeToRefresh.java
@@ -0,0 +1,41 @@
+package com.alperez.samples.safeswiperefresh.widget;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+/**
+ * Created by stanislav.perchenko on 10/12/2018
+ */
+public class CustomSwipeToRefresh extends SwipeRefreshLayout {
+    private int mTouchSlop;
+    private float mPrevX;
+
+    public CustomSwipeToRefresh(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPrevX = MotionEvent.obtain(event).getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                final float eventX = event.getX();
+                float xDiff = Math.abs(eventX - mPrevX);
+
+                if (xDiff > mTouchSlop) {
+                    return false;
+                }
+        }
+
+        return super.onInterceptTouchEvent(event);
+    }
+}
